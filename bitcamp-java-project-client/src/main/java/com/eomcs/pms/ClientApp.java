4b74c066ddb635ba08fcb7b1982bce49bc7804af
shin-so-mi <thaleovud1@naver.com>
2020-10-14 17:55:44 +0900

    백업

--- bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -5,33 +5,64 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import com.eomcs.util.Prompt;
 
 //stateful
 // ->  서버와 연결하여 간단한 메세지 주고받기
 public class ClientApp {
     public static void main(String[] args) {
-     //서버주소 :localhost
-      //서버포트 : 8888
-       //서버와 연결된소켓을 생성한다
-      //소켓을 통헤 문자열을 입출력 할 수 있도록 스트림 객체를 준비한다
-      //먼저 서버에 간단한 인사말을 보낸다
-      //서버가 응답한 메세지를 출력한다
-
-      try (Socket socket = new Socket("localhost", 8888);
+     if (args.length != 2) {
+       System.out.println("프로그램 사용");
+       System.out.println(" java - cp ... clientApp 서버주소 포트번호");
+       System.exit(0);
+     }
+     //클라이언트가 서버에 스탑 명력을 보내면 다음변수를  true로 변경한다
+     boolean stop = false;
+      try (Socket socket = new Socket(args[0],Integer.parseInt(args[1]));
           PrintStream out = new PrintStream(socket.getOutputStream());
           BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
 
-        out.println("hello");
+
+        while (true) {
+        String input = Prompt.inputString("명령> ");
+        out.println(input);
         out.flush();
 
+        receiveResponse(in);
 
-        String response = in.readLine();
-        System.out.println(response);
 
+        if (input.equalsIgnoreCase("quit")) {
+          break;
+        } else if (input.equalsIgnoreCase("stop")) {
+        break;
+        }
+        }
 
       } catch (Exception e) {
         e.printStackTrace();
       }
+
+      if (stop) {
+        ///서버를 멈추기 위해 그냥 접속했다가 끊는다
+        try (Socket socket = new Socket(args[0], Integer.parseInt(args)[1])) {
+
+        }
+      }
 }
+    private static void receiveResponse(BufferedReader in) throws Exception {
+      while (true) {
+        String response = in.readLine();
+        if (response.length() == 0)
+          break;
+        System.out.println(response);
+      }
 }
+//반복해서 클라이언트의 요청을 처리한다
+//
+}
+
+
+
+
+
