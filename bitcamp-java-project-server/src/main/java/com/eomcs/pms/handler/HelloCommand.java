ddf01ce1c62a126ad148873f4653a1199af6e849
shin-so-mi <thaleovud1@naver.com>
2020-11-20 18:40:06 +0900

    백

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/handler/HelloCommand.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/handler/HelloCommand.java
@@ -1,13 +1,50 @@
 package com.eomcs.pms.handler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
-// Command 규칙에 따라 클래스를 정의한다.
-public class HelloCommand implements Command {
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
-  @Override
-  public void execute(PrintWriter out, BufferedReader in) {
-    out.println("안녕하세요!");
-  }
+ @WebServlet("hello")
+ 
+public class HelloCommand implements Servlet {
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
