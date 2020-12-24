27ca7fa007a71558205a12432961e889cb1cf667
shin-so-mi <shin-so-mi>
2020-12-24 17:49:35 +0900

    백업

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/service/impl/DefaultMemberService.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/service/impl/DefaultMemberService.java
@@ -1,9 +1,11 @@
-package com.eomcs.pms.service;
+package com.eomcs.pms.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
+import com.eomcs.pms.service.MemberService;
 
 @Service
 public class DefaultMemberService implements MemberService {
@@ -46,7 +48,11 @@ public class DefaultMemberService implements MemberService {
 
   @Override
   public Member get(String email, String password) throws Exception {
-    return memberDao.findByEmailPassword(email, password);
+    HashMap<String,Object> map = new HashMap<>();
+    map.put("email", email);
+    map.put("password", password);
+
+    return memberDao.findByEmailPassword(map);
   }
 
   @Override
